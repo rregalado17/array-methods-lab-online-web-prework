c3dea04e1689a8dfe4e468e34a5228f5f8229e35
magical-flow-3186 <rregalado17@gmail.com>
2019-06-03 21:39:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   element1 = "wow"
   element2 = "Tardis"
-  array.include?(element1)
+  array.include?
   array.include?(element2)
 end
 
