e406377e3b745f2dbdfde110c570ae7e5930cda3
magical-flow-3186 <rregalado17@gmail.com>
2019-06-03 21:38:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,8 +2,8 @@ def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   element1 = "wow"
   element2 = "Tardis"
-  array.include?()
-
+  array.include?(element1)
+  array.include?(element2)
 end
 
 def using_sort(array)
